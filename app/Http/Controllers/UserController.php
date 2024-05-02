3c3ae0d8db1cf864df4aa51c94ec03004cf49006
wajidalibitsclan <wajidali.bitsclan@gmail.com>
2024-05-02 11:58:38 +0500

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Users/Index', [
+            'users' => User::paginate(10)
+        ]);
+    }
+}
---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,69 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head, Link } from "@inertiajs/vue3";
+
+defineProps({
+  users: {
+    type: Object,
+  },
+});
+</script>
+
+<template>
+  <Head title="Users Management" />
+
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        User Management
+      </h2>
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <div class="py-3 mx-3 my-4">
+            <Link
+              :href="route('users')"
+              class="py-3 px-4 rounded-sm bg-red-400 text-white"
+              >Create User</Link
+            >
+          </div>
+          <table
+            class="min-w-full divide-y divide-gray-200 bg-white shadow-md rounded-lg"
+          >
+            <thead class="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  ID
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Email
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200">
+              <tr v-for="user in users.data" :key="user">
+                <td class="px-6 py-4 whitespace-nowrap">{{ user.id }}</td>
+                <td class="px-6 py-4 whitespace-nowrap">{{ user.name }}</td>
+                <td class="px-6 py-4 whitespace-nowrap">{{ user.email }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </AuthenticatedLayout>
+</template>
